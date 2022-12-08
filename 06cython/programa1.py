3c8796491bf92d3110be27df46f6663d991c807d
rodskades <rodolpho_kades@hotmail.com>
2022-12-08 14:34:13 -0300

    Learning cython

---
+++ 06cython/programa1.py
@@ -0,0 +1,10 @@
+import cumprimenta
+
+
+def main():
+    nome: str = input('Qual o seu nome? ')
+    cumprimenta.cumprimentar(nome)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 06cython/setup.py
@@ -0,0 +1,12 @@
+"""
+Preparando o build a ser feito com:
+
+python setup.py build_ext --inplace
+"""
+
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(['cumprimenta.pyx'])
+)
